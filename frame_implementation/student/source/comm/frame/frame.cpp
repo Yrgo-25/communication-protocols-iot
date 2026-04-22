753be2340ed47f650458c3c0b93bd9916117e7da
Erik Pihl <erik.axel.pihl@gmail.com>
2026-04-22 09:00:12 +0200

    Update documentation

---
+++ frame_implementation/student/source/comm/frame/frame.cpp
@@ -0,0 +1,9 @@
+/**
+ * @brief Frame implementation details.
+ */
+#include <cstddef>
+#include <cstdint>
+
+#include "comm/frame/frame.h"
+#include "comm/frame/offset.h"
+#include "comm/frame/type.h"
